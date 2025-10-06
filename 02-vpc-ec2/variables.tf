3ce0b05009957aa20aa0501b86e1c37fb0392553
Rahul bollu <bvrahul3141@gmail.com>
2025-10-06 21:33:12 +0530

    UPDATED image ami id

--- 02-vpc-ec2/variables.tf
+++ 02-vpc-ec2/variables.tf
@@ -7,5 +7,5 @@ variable "instance_type" {
 variable "ami_id" {
   description = "AMI ID for Amazon Linux 2 in us-east-1"
   type        = string
-  default     = "ami-0c55b159cbfafe1f0"
+  default     = "ami-0360c520857e3138f"
 }
