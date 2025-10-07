929e17b76cd213306a6514b735b751e85b2b1d06
Rahul Bollu <bvrahul3141@gmail.com>
2025-10-07 13:49:29 +0530

    updated main.tf

--- 03-eks-cluster/main.tf
+++ 03-eks-cluster/main.tf
@@ -1,38 +1,22 @@
-###########################################
-# AWS Provider
-###########################################
-
 provider "aws" {
   region = "us-east-1"
 }
 
-###########################################
-# EKS Cluster using Terraform AWS Module
-###########################################
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "17.24.0" # specify a version to avoid unexpected changes
+  version = "~> 21.0"
 
   name               = var.cluster_name
   kubernetes_version = var.cluster_version
   vpc_id             = var.vpc_id
   subnet_ids         = var.subnets
 
-  eks_managed_node_groups = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-      instance_types   = ["t3.medium"]
-      labels = {
-        role = "worker"
-      }
-      tags = {
-        Name = "terraform-eks-node-group"
-      }
-    }
+  compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
   }
+
+  enable_cluster_creator_admin_permissions = true
 
   tags = {
     Environment = "dev"
