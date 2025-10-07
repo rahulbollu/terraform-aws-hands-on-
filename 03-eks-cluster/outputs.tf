61ee74ad497aed59ef9da2d251e06314b754b32d
Rahul Bollu <bvrahul3141@gmail.com>
2025-10-07 15:47:13 +0530

    updated output.tf file

--- 03-eks-cluster/outputs.tf
+++ 03-eks-cluster/outputs.tf
@@ -1,16 +1,19 @@
 output "cluster_name" {
- description = "Name of the EKS cluster"
- value       = module.eks.cluster_name
+  description = "Name of the EKS cluster"
+  value       = module.eks.cluster_name
 }
+
 output "cluster_endpoint" {
- description = "EKS cluster endpoint"
- value       = module.eks.cluster_endpoint
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
 }
+
 output "cluster_oidc_issuer" {
- description = "OIDC issuer URL for the EKS cluster"
- value       = module.eks.oidc_provider
+  description = "OIDC issuer URL for the EKS cluster"
+  value       = module.eks.oidc_provider
 }
+
 output "node_pool_names" {
   description = "List of node pool names"
-  value       = module.eks.compute.node_pool_names
+  value       = module.eks.node_pool_names
 }
