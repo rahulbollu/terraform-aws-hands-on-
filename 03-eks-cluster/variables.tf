a1c9de3cb8add2a90444b39fbb0f86162a8d3101
Rahul Bollu <bvrahul3141@gmail.com>
2025-10-07 13:34:03 +0530

    updated variables.tf

--- 03-eks-cluster/variables.tf
+++ 03-eks-cluster/variables.tf
@@ -11,8 +11,10 @@ variable "cluster_version" {
 variable "vpc_id" {
  description = "VPC ID where EKS will be created"
  type        = string
+ default     = "vpc-027a7dc2e73d24a8c"
 }
 variable "subnets" {
  description = "Subnets for worker nodes"
  type        = list(string)
+ default =  "subnet-0d6371041900796a1" 
 }
