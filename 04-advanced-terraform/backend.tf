b750e501762f45dd2c0166a0e36f20d4cf33a83e
Rahul bollu <bvrahul3141@gmail.com>
2025-10-09 12:42:58 +0530

    updated

--- 04-advanced-terraform/backend.tf
+++ 04-advanced-terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-demo-new"
     key            = "global/s3/terraform.tfstate"
-    region         = "us-west-2"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
--- 04-advanced-terraform/main.tf
+++ 04-advanced-terraform/main.tf
@@ -1,25 +1,44 @@
+###########################################
+# AWS Provider
+###########################################
+provider "aws" {
+  region = var.region
+}
+
+###########################################
+# VPC Module
+###########################################
 module "vpc" {
   source             = "./modules/vpc"
   region             = var.region
   project_name       = var.project_name
-  public_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
-  availability_zones = ["us-east-1a", "us-east-1b"]
+  vpc_cidr           = var.vpc_cidr
+  public_subnets     = var.public_subnets
+  availability_zones = var.availability_zones
 }
 
+###########################################
+# EC2 Module
+###########################################
 module "ec2" {
   source            = "./modules/ec2"
   region            = var.region
   project_name      = var.project_name
-  subnet_id         = module.vpc.public_subnet_ids[0]
-  security_group_id = module.vpc.default_sg_id
+  subnet_id         = module.vpc.public[0].id
+  security_group_id = module.vpc.sg_id
   ami_id            = var.ami_id
+  instance_type     = var.instance_type
 }
 
+###########################################
+# EKS Module
+###########################################
 module "eks" {
-  source         = "./modules/eks"
-  region         = var.region
-  project_name   = var.project_name
-  vpc_id         = module.vpc.vpc_id
-  subnet_ids     = module.vpc.public_subnet_ids
-  cluster_name   = var.cluster_name
+  source          = "./modules/eks"
+  region          = var.region
+  project_name    = var.project_name
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.public_subnet_ids
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
 }
