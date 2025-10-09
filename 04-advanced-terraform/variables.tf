068dc7b078cf525f220d2123e431c645d1d6bf6e
Rahul Bollu <bvrahul3141@gmail.com>
2025-10-09 19:22:20 +0530

    updated region and ami id for ec2 module

--- 04-advanced-terraform/variables.tf
+++ 04-advanced-terraform/variables.tf
@@ -6,7 +6,7 @@
 variable "region" {
   description = "AWS region where infrastructure will be deployed"
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-2"
 }
 
 # Project Name
@@ -43,7 +43,7 @@ variable "availability_zones" {
 variable "ami_id" {
   description = "AMI ID for EC2 instance (Amazon Linux 2 in us-east-1)"
   type        = string
-  default     = "ami-0360c520857e3138f"
+  default     = "ami-03aa99ddf5498ceb9"
 }
 
 variable "instance_type" {
