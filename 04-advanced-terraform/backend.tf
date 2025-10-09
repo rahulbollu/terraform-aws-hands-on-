0ad1f3ae5cd9ab8d163f6f9cbdfcd1c78c0ceee0
Rahul bollu <bvrahul3141@gmail.com>
2025-10-09 12:46:37 +0530

    updated location

--- 04-advanced-terraform/backend.tf
+++ 04-advanced-terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-demo-new"
     key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-west-2"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
