ea499c85f8077b911b08a0cf5f69360cd20681d6
Rahul Bollu <bvrahul3141@gmail.com>
2025-10-09 19:10:10 +0530

    changed ami id

--- test-ec2.tf
+++ test-ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "test" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  ami           = "ami-03aa99ddf5498ceb9" # Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t2.micro"
 
   tags = {
